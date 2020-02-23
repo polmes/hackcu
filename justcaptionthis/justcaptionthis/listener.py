e70629d17c6fb18809621a741040f6a9f97e492f
Jose A. Santamaría <josealejandros.95@gmail.com>
2020-02-22 20:43:14 -0700

    Use DenseCap API
    Read the URL of the first image of a tweet mentioning @JustCaptionThis, pass it to DenseCap API and print the first guess.

--- justcaptionthis/justcaptionthis/listener.py
+++ justcaptionthis/justcaptionthis/listener.py
@@ -1,6 +1,23 @@
 import tweepy
+import requests
 
 class MentionListener(tweepy.StreamListener):
 
 	def on_status(self, status):
 		print(status.text)
+		img = status.extended_entities['media'][0]['media_url_https']
+
+		headers = {
+		    'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K',
+		}
+
+		files = {
+		    'image': img,
+		}
+
+		response = requests.post('https://api.deepai.org/api/densecap', headers=headers, files=files).json()
+
+		caption = response['output']['captions'][0]['caption']
+
+		# print(response)
+		print(caption)
