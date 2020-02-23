8aa08b748be6c472deb9a87962399e225410b209
polmes <pol.developer1@gmail.com>
2020-02-23 00:09:20 -0700

    Recursively call on_status to search for parent image

--- justcaptionthis/justcaptionthis/listener.py
+++ justcaptionthis/justcaptionthis/listener.py
@@ -8,21 +8,24 @@ class MentionListener(tweepy.StreamListener):
 		self.key = deepai['key']
 
 	def on_status(self, status):
+		# If tweet has an image, tag this one
 		if hasattr(status, 'extended_entities'):
 			print(status.text)
 
 			# Get first image url in tweet
 			img = status.extended_entities['media'][0]['media_url_https']
 
-			# Call DenseCap API
+			# Setup DenseCap
 			headers = {
 				'api-key': self.key,
 			}
 
+			# Image URL
 			files = {
 				'image': img,
 			}
 
+			# Call DenseCap API
 			response = requests.post('https://api.deepai.org/api/densecap', headers=headers, files=files).json()
 
 			# Take first caption provided
@@ -35,3 +38,11 @@ class MentionListener(tweepy.StreamListener):
 
 				# Tweet (reply) the response
 				self.api.update_status(caption, in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)
+
+		# If tweet has no image, try and find parent tweet with image
+		else:
+			if status.in_reply_to_status_id is not None:
+				parent = self.api.get_status(status.in_reply_to_status_id)
+
+				# Recursive call
+				self.on_status(parent)
