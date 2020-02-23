190a93af9bebea29b0dfc612344af11408c3a24d
polmes <pol.developer1@gmail.com>
2020-02-23 03:18:55 -0700

    Add support for tweets with multiple images

--- justcaptionthis/justcaptionthis/listener.py
+++ justcaptionthis/justcaptionthis/listener.py
@@ -16,32 +16,43 @@ class MentionListener(tweepy.StreamListener):
 		if hasattr(status, 'extended_entities'):
 			print(status.text)
 
-			# Get first image url in tweet
-			img = status.extended_entities['media'][0]['media_url_https']
+			# Check number of images on tweet
+			# multiple = True if len(status.extended_entities['media']) > 1 else False
+			# img = status.extended_entities['media'][0]['media_url_https']
 
 			# Setup DenseCap
 			headers = {
 				'api-key': self.key,
 			}
 
-			# Image URL
-			files = {
-				'image': img,
-			}
+			caption = []
+			for each in status.extended_entities['media']:
+				# Get image URL
+				img = each['media_url_https']
+				files = {
+					'image': img,
+				}
 
-			# Call DenseCap API
-			response = requests.post('https://api.deepai.org/api/densecap', headers=headers, files=files).json()
+				# Call DenseCap API
+				response = requests.post('https://api.deepai.org/api/densecap', headers=headers, files=files).json()
 
-			# Take first caption provided
-			if 'output' in response:
-				caption = response['output']['captions'][0]['caption']
-				# caption = f"@{status.author.screen_name} {response['output']['captions'][0]['caption']}"
+				# Take first caption provided
+				if 'output' in response:
+					caption.append(response['output']['captions'][0]['caption'])
 
-				# print(response)
-				print(caption)
+
+			if caption:
+				# Build tweet
+				if len(caption) == 1:
+					tweet = caption[0]
+				else:
+					tweet = ''
+					for i, c in enumerate(caption):
+						tweet += f'[{i+1}] {c}\n'
+				print(tweet)
 
 				# Tweet (reply) the response
-				self.api.update_status(caption, in_reply_to_status_id=self.id, auto_populate_reply_metadata=True)
+				self.api.update_status(tweet, in_reply_to_status_id=self.id, auto_populate_reply_metadata=True)
 
 		# If tweet has no image, try and find parent tweet with image
 		else:
